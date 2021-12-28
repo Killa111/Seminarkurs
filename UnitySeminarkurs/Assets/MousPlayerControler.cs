ad1cd4315698a63b6cd179305a4a838ea7de56c0
Killa111 <nur.felix@web.de>
2021-12-28 16:53:09 +0100

    MoveRange added

--- UnitySeminarkurs/Assets/MousPlayerControler.cs
+++ UnitySeminarkurs/Assets/MousPlayerControler.cs
@@ -6,7 +6,11 @@ public class MousPlayerControler : MonoBehaviour
 
     public Camera mainCam;
 
+    public Transform posCam;
+
     public NavMeshAgent meshAgent;
+
+    public string nameMoveRange;
 
     // Update is called once per frame
     void Update()
@@ -15,16 +19,37 @@ public class MousPlayerControler : MonoBehaviour
         {
             Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);    // create a ray, from the mainCam,
                                                                         // in the dirrection of the current mouseposition
-            RaycastHit hit;                                             // RaycastHit stores what got hit
+            RaycastHit[] hits;                                          // RaycastHit[] stores everything the ray goes through
+            RaycastHit hitUse = new RaycastHit();                       // the hit that is used                                     
+            bool foundField = false;                                    // got an Field hit
+            bool foundMoveRange = false;                                // got the MoveRange hit
 
-            if (Physics.Raycast(ray, out hit))                          // shoots ot the ray, stors what got hit
-                                                                        // in hit, if something got hit continue in if                                                                        
+            hits = Physics.RaycastAll(ray);                             // shoots ot the ray, stores everything it hits in hits
+
+            float nearestHitField = Mathf.Infinity;                     // distance to nearest hit Field
+            
+            for(int i = 0; i < hits.Length; i++)                        // for loop goes through all the hits stored in hit
             {
-                if(hit.collider.tag == "Field")
+                if(hits[i].transform.tag == "Field")                    // checks if a object with the tag Field got hit
                 {
-                    meshAgent.SetDestination(hit.point);
+                    if(Vector3.Distance(posCam.position, hits[i].point) < nearestHitField) // checks if the Field is the nearest
+                    {
+                        hitUse = hits[i];
+                        nearestHitField = Vector3.Distance(posCam.position, hits[i].point);
+                        foundField = true;
+                    }
                 }
+
+                if(hits[i].transform.name == nameMoveRange)             // checks if the MoveRange got hit
+                {
+                    foundMoveRange = true;
+                }                
             }
+
+            if(foundField == true & foundMoveRange == true)        
+            {
+                meshAgent.SetDestination(hitUse.point);
+            }            
         }
     }
 }
