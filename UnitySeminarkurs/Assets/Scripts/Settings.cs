19d5da7c4b306f3edb35f32f2d386c0a90d1bbbb
Killa111 <nur.felix@web.de>
2022-07-12 18:13:04 +0200

    settings updated

--- UnitySeminarkurs/Assets/Scripts/Settings.cs
+++ UnitySeminarkurs/Assets/Scripts/Settings.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject pannelAudio;
+    public GameObject pannelVideo;
+    public GameObject pannelSteuerung;
+
+    public GameObject buttonAudio;
+    public GameObject buttonVideo;
+    public GameObject buttonSteuerung;
+
+    private Color standardColor;
+    private Color activatetColor;
+
+    private void deactivateAll()
     {
-        
+        buttonVideo.GetComponent<Image>().color = standardColor;
+        buttonSteuerung.GetComponent<Image>().color = standardColor;
+        buttonAudio.GetComponent<Image>().color = standardColor;
+        pannelVideo.SetActive(false);
+        pannelSteuerung.SetActive(false);
+        pannelAudio.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void activateAudioSettings()
     {
-        
+        deactivateAll();
+
+        buttonAudio.GetComponent<Image>().color = activatetColor;
+        pannelAudio.SetActive(true);
+    }
+
+    public void activateSteuerungSettings()
+    {
+        deactivateAll();
+
+        buttonSteuerung.GetComponent<Image>().color = activatetColor;
+        pannelSteuerung.SetActive(true);
+    }
+
+    public void activateVideoSettings()
+    {
+        deactivateAll();
+
+        buttonVideo.GetComponent<Image>().color = activatetColor;
+        pannelVideo.SetActive(true);
+    }
+
+
+    private void Start()
+    {
+        ColorUtility.TryParseHtmlString("D6D6D6", out standardColor);
+        ColorUtility.TryParseHtmlString("969696", out activatetColor);
+
+        activateAudioSettings();
     }
 }
